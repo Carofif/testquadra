7f89bd54b4a8ea36d980cbc118aa07ab67057a76
Carofif <carollebonou@gmail.com>
2019-10-02 11:10:27 +0100

    alignement

--- src/components/article/Article.vue
+++ src/components/article/Article.vue
@@ -1,17 +1,21 @@
 <template>
 <div>
-  <router-link class="navbar-item has-text-dark" to="/categorie">
-    <span>Aide en ligne/Catégorie</span>
-  </router-link>
-  <div class="card" id="card" v-if="getArticles.length">
-    <strong class="card-header-title" style="font-size: 3em; margin: auto">{{article.titre}}</strong>
-    <div class="card-content" v-html="article.content"></div>
-    <div class="card" id="card-foot">
-        <span class="card-content" id="footer"> Ces informations vous-ont elles été utiles?
-          <img src="./../../assets/images/smiling.png" alt="">
-          <img src="./../../assets/images/confused.png" alt="">
-          <img src="./../../assets/images/unhappy.png" alt="">
-        </span>
+  <div class="columns is mobible">
+    <div class="column is-three-fifths is-offset-one-fifth">
+      <router-link class="navbar-item has-text-dark" to="/categorie">
+        <span>Aide en ligne/Catégorie</span>
+      </router-link>
+      <div class="card" id="card" v-if="getArticles.length">
+        <strong class="card-header-title" style="font-size: 3em; margin: auto">{{article.titre}}</strong>
+        <div class="card-content" v-html="article.content"></div>
+        <div class="card" id="card-foot">
+            <span class="card-content" id="footer"> Ces informations vous-ont elles été utiles?
+              <img src="./../../assets/images/smiling.png" alt="">
+              <img src="./../../assets/images/confused.png" alt="">
+                <img src="./../../assets/images/unhappy.png" alt="">
+            </span>
+        </div>
+      </div>
     </div>
   </div>
 </div>
